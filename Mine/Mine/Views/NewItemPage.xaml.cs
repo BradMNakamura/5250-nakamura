86a767f8034293f3f5c861df6f291a80abdc21e4
nakamurabrad@seattleu.edu <70090205+BradMNakamura@users.noreply.github.com>
2022-01-10 13:55:36 -0800

    Object value now changes when updated. Added comment to document Value_OnStepperValuecChanged in NewItemPage

--- Mine/Mine/Views/NewItemPage.xaml.cs
+++ Mine/Mine/Views/NewItemPage.xaml.cs
@@ -39,9 +39,14 @@ namespace Mine.Views
             await Navigation.PopModalAsync();
         }
 
+
+        /// <summary>
+        /// Update the Display Value when the Stepper Changes
+        /// </summary>
         void Value_OnStepperValueChanged(object sender, ValueChangedEventArgs e)
         {
             ValueValue.Text = String.Format("{0}", e.NewValue);
+            Item.Value = (int)e.NewValue;
         }
     }
 }
