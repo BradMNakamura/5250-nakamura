5c7ae37c202762d07dec4ca3b35d4a5cbcadc015
nakamurabrad@seattleu.edu <70090205+BradMNakamura@users.noreply.github.com>
2022-01-31 21:27:32 -0800

    Added IndexAsync in DataBaseService.cs

--- Mine/Mine/Services/DatabaseService.cs
+++ Mine/Mine/Services/DatabaseService.cs
@@ -57,9 +57,16 @@ namespace Mine.Services
             throw new NotImplementedException();
         }
 
-        public Task<IEnumerable<ItemModel>> IndexAsync(bool forceRefresh = false)
+        /// <summary>
+        /// Return all items from database as a list.
+        /// </summary>
+        /// <param name="forceRefresh"></param>
+        /// <returns></returns>
+        public async Task<IEnumerable<ItemModel>> IndexAsync(bool forceRefresh = false)
         {
-            throw new NotImplementedException();
+            //Get all items from database as a async list.
+            var result = await Database.Table<ItemModel>().ToListAsync(); 
+            return result;
         }
     }
 }
