1de6b5ff1ae0a5cbaea20533b3a17fcb1b645f4c
nakamurabrad@seattleu.edu <70090205+BradMNakamura@users.noreply.github.com>
2022-02-06 21:51:10 -0800

    Added UT for HomeMenuItem to HomeMenuItemTests.cs

---
+++ UnitTests/Models/HomeMenuItemTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using Mine.Models;
+namespace UnitTests.Models
+{
+    //Arrange 
+    //Act
+    //Reset
+    //Assert
+    [TestFixture]
+    public class HomeMenuItemTests
+    {
+        /*
+        [Test]
+        public void ItemModel_
+        {
+          //Arrange 
+            //Act
+            //Reset
+            //Assert
+        }
+        */
+        public void HomeMenuItems_Constructor_Valid_Default_Should_Pass()
+        {
+            // Arrange
+            // Act
+            var result = new HomeMenuItem();
+
+            // Reset
+
+            // Assert 
+            Assert.IsNotNull(result);
+        }
+
+        [Test]
+        public void HomeMenuItems_Set_Get_Valid_Default_Should_Pass()
+        {
+            //Arrange 
+            //Act
+            var result = new HomeMenuItem();
+            result.Id = MenuItemType.Items;
+            result.Title = "Title";
+
+            //Reset
+
+            //Assert
+            Assert.AreEqual(MenuItemType.Items, result.Id);
+            Assert.AreEqual("Title", result.Title);
+        }
+
+        [Test]
+        public void HomeMenuItems_Get_Valid_Default_Should_Pass() 
+        {
+            //Arrange 
+            //Act
+            var result = new HomeMenuItem();
+            //Reset
+            //Assert
+            Assert.AreEqual(null, result.Id);
+            Assert.AreEqual(null, result.Title);
+        }
+    }
+}
